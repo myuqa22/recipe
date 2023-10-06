2a437749cb9d09b5153aa021d40254131955f832
glukman <52576264+myuqa22@users.noreply.github.com>
2023-10-06 18:21:25 +0200

    add initial project structure

---
+++ Recipe/Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Privat on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Recipe/RecipeTests/RecipeTests.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeTests.swift
+//  RecipeTests
+//
+//  Created by Privat on 06.10.23.
+//
+
+import XCTest
+
+final class RecipeTests: XCTestCase {
+
+    override func setUpWithError() throws {}
+
+    override func tearDownWithError() throws {}
+
+    func testExample() throws {}
+
+}
