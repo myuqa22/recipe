b2d33beee748d798cd0d8fe29260a0077b740552
glukman <52576264+myuqa22@users.noreply.github.com>
2023-10-08 18:03:00 +0200

    add init to service

--- Recipe/Recipe/Helper/Edamam API/EdamamService.swift
+++ Recipe/Recipe/Helper/Edamam API/EdamamService.swift
@@ -18,18 +18,22 @@ class EdamamService: EdamamServiceProtocol {
     let appId = "6cc7056b"
     let appKey = "9425d5f01318992465170a489abceb56"
     let type = "public"
-    
     var url = URL(string: "https://api.edamam.com/api/recipes/v2?")!
     
-    func getRecipesBy(query: String) async throws -> RecipesDto? {
+    init() {
         
         let queryItems = [
             URLQueryItem(name: "app_id", value: appId),
             URLQueryItem(name: "app_key", value: appKey),
-            URLQueryItem(name: "q", value: query),
             URLQueryItem(name: "type", value: type)
         ]
-        url.append(queryItems: queryItems)
+        
+        self.url.append(queryItems: queryItems)
+    }
+    
+    func getRecipesBy(query: String) async throws -> RecipesDto? {
+        
+        self.url.append(queryItems: [ URLQueryItem(name: "q", value: query)])
         
         let (data, _) = try await URLSession.shared.data(from: url)
         let decoded = try JSONDecoder().decode(RecipesDto.self, from: data)
